e5256296dcedf5912b7f248bbbb11fa24cc612fd
Timofey-Vodyanov <timofeyvodyanov@gmail.com>
2025-10-21 18:20:49 +0300

    сделал возможность победить и закомитил

--- TicTacToe2/Form1.cs
+++ TicTacToe2/Form1.cs
@@ -30,6 +30,46 @@ namespace TicTacToe2
                 bt.Image = images[1];
                 label1.Text = "Ход: Игрок 1 (LoL)";
             }
+            Check();
+        }
+
+        private void deactivate()
+        {
+            foreach (Control X in this.Controls)
+            {
+                if (X is Button)
+                {
+                    ((Button)X).Enabled = false;
+                }
+            }
+        }
+        private void Check()
+        {
+            if (button1.Image == images[0] && button2.Image == images[0] && button3.Image == images[0]
+               || button4.Image == images[0] && button5.Image == images[0] && button6.Image == images[0]
+               || button7.Image == images[0] && button9.Image == images[0] && button8.Image == images[0]
+               || button1.Image == images[0] && button4.Image == images[0] && button7.Image == images[0]
+               || button2.Image == images[0] && button5.Image == images[0] && button8.Image == images[0]
+               || button3.Image == images[0] && button6.Image == images[0] && button9.Image == images[0]
+               || button1.Image == images[0] && button5.Image == images[0] && button9.Image == images[0]
+               || button3.Image == images[0] && button5.Image == images[0] && button7.Image == images[0])
+            {
+                MessageBox.Show("Победила League of Legends!");
+                deactivate();
+            }
+
+            else if (button1.Image == images[1] && button2.Image == images[1] && button3.Image == images[1]
+            || button4.Image == images[1] && button5.Image == images[1] && button6.Image == images[1]
+            || button7.Image == images[1] && button9.Image == images[1] && button8.Image == images[1]
+            || button1.Image == images[1] && button4.Image == images[1] && button7.Image == images[1]
+            || button2.Image == images[1] && button5.Image == images[1] && button8.Image == images[1]
+            || button3.Image == images[1] && button6.Image == images[1] && button9.Image == images[1]
+            || button1.Image == images[1] && button5.Image == images[1] && button9.Image == images[1]
+            || button3.Image == images[1] && button5.Image == images[1] && button7.Image == images[1])
+            {
+                MessageBox.Show("Победила Dota 2!");
+                deactivate();
+            }
         }
 
         private void label1_Click(object sender, EventArgs e)
